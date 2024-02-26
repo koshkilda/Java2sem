6e81b84631fd099f706531926668350ab5d38f5a
koshkilda <89313188+koshkilda@users.noreply.github.com>
2024-02-26 17:49:09 +0300

    Add files via upload

---
+++ AreaUnderTheGraph.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class AreaUnderTheGraph {
+    public static double f (double x) {
+        return x * x * x + x + 1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        sc.close();
+
+        double period = 0.1; 
+        double k = 0.5;
+        double x = 0;
+        double y = 0;
+        double area = 0;
+
+        while (k * period < b - a) {
+            x = a + k * period;
+            y = f(x);
+            area += period * y;
+            k++;
+        }
+        
+        System.out.format("%.3f", area);
+    }
+}
