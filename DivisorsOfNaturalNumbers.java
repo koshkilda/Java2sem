cb86aa286e08c78d951b4fc38b6de143aedd9043
koshkilda <89313188+koshkilda@users.noreply.github.com>
2024-04-07 16:58:03 +0300

    changed to simple factorization

--- DivisorsOfNaturalNumbers.java
+++ DivisorsOfNaturalNumbers.java
@@ -1,6 +1,30 @@
 import java.util.Scanner;
 
 class DivisorsOfNaturalNumbers {
+    public static int number_of_divisors(int n) {
+        int number_of_divisors = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                number_of_divisors++;
+            }
+        }
+        
+        return number_of_divisors;
+    }
+    
+    
+    public static int divisors_power(int divisor, int number) {
+        int divisors_power = 1;
+    
+        while (number / Math.pow(divisor, divisors_power) % divisor == 0) {
+            divisors_power++;
+        }
+        
+        return divisors_power;
+    } 
+    
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -8,7 +32,9 @@ class DivisorsOfNaturalNumbers {
 
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
-                System.out.println(i + " является делителем числа " + n);
+                if (number_of_divisors(i) == 2) {
+                    System.out.println(i + "^" + divisors_power(i, n) + " является простым множителем числа " + n);
+                }
             }
         }
     }
